62aa63ef3f69cf3559f577c80d84434337d33619
floretn <ivanse99@yandex.ru>
2025-04-04 00:43:00 +0300

    test commit

---
+++ src/main/java/ru/mephi/cb/controllers/GameController.java
@@ -0,0 +1,13 @@
+package ru.mephi.cb.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GameController {
+
+    @GetMapping("/game")
+    public String game() {
+        return "game";
+    }
+}
